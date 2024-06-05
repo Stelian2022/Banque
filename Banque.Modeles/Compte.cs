d710800e1f6cca4e49f8039d1ffe6daf12af3d7c
Stelian2022 <stanciu.stelian86@yahoo.fr>
2024-06-05 10:24:03 +0200

    Ajoutez des fichiers projet.

---
+++ Banque.Modeles/Compte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque.Modeles
+{
+    public class Compte
+    {
+     
+       public string Proprietaire { get; set; }
+
+        public void Crediter(decimal montant)
+        {          
+        }
+
+        public void Debiter(decimal montant)
+        {
+        }
+
+        public void Debiter(decimal montant, Compte compte)
+        {
+            Debiter(montant);
+            compte.Crediter(montant);
+        }
+    }
+}
---
+++ Banque/Program.cs
@@ -0,0 +1,28 @@
+﻿using Banque.Modeles;
+
+namespace Banque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compte compteCourantNicolas = new Compte();
+            compteCourantNicolas.Proprietaire = "Nicolas";
+
+            Compte compteEpargneNicolas = new Compte();
+            compteEpargneNicolas.Proprietaire = "Nicolas";
+
+            Compte compteCourantJeremy = new Compte();
+            compteCourantJeremy.Proprietaire = "Jeremy";
+
+            compteCourantNicolas.Crediter(100m);
+            compteCourantNicolas.Debiter(50m);
+            compteCourantNicolas.Debiter(20m, compteEpargneNicolas);
+
+            Console.WriteLine($"Compte courant de  {compteCourantNicolas.Proprietaire}");
+            Console.WriteLine($"Compte epargne de  {compteEpargneNicolas.Proprietaire}");
+            Console.WriteLine($"Compte courant de  {compteCourantJeremy.Proprietaire}");
+
+        }
+    }
+}
